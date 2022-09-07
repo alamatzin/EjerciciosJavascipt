0d9034bc095bbd450a5a1e6db92a76ea253546bf
Alam Atzin <alamatzin@ciencias.unam.mx>
2022-09-07 14:08:27 -0500

    Ej5.1 resuelto

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
-let arreglo1 = [1,22,5,17,10,5,40,5]
+let btn = document.getElementById("btnVal");
+let campo1 = document.getElementById("n1");
+let campo2 = document.getElementById("n2");
+let campo3 = document.getElementById("n3");
+let resul = document.getElementById("alerta");
 
-function numRepetido(arreglo){
-    for (let i = 0; i < array.length; i++) {
-        
-        
-    }
-}
+btn.addEventListener("click", function(e){
+    e.preventDefault();
+    if (campo1.value > campo2.value && campo1.value > campo3.value ){
+        resul.innerHTML = `El numero mayor es ${campo1.value}`
+    } 
+    if (campo2.value > campo1.value && campo2.value > campo3.value ){
+        resul.innerHTML = `El numero mayor es ${campo2.value}`
+    } 
+    if (campo3.value > campo1.value && campo3.value > campo2.value ){
+        resul.innerHTML = `El numero mayor es ${campo3.value}`
+    } 
+
+    if (campo1.value < 101 && campo1.value > 0){
+        campo1.classList.remove("is-invalid");
+        campo1.classList.add("is-valid");
+    } else {
+        campo1.classList.remove("is-valid");
+        campo1.classList.add("is-invalid");
+    }//campo1
+    if (campo2.value < 101 && campo2.value > 0){
+        campo2.classList.remove("is-invalid");
+        campo2.classList.add("is-valid");
+    } else {
+        campo2.classList.remove("is-valid");
+        campo2.classList.add("is-invalid");
+    }//campo2
+    if (campo3.value < 101 && campo3.value > 0){
+        campo3.classList.remove("is-invalid");
+        campo3.classList.add("is-valid");
+    } else {
+        campo3.classList.remove("is-valid");
+        campo3.classList.add("is-invalid");
+    }//campo3
+
+    
+})
