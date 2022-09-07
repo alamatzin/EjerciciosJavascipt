64d8268e1f2cd0270ffc13e2ddc876f7f59aa18b
Alam Atzin <alamatzin@ciencias.unam.mx>
2022-09-07 14:28:36 -0500

    Ej5.4 resuelto

--- js/main.js
+++ js/main.js
@@ -1,42 +1,22 @@
 let btn = document.getElementById("btnVal");
 let campo1 = document.getElementById("n1");
-let campo2 = document.getElementById("n2");
-let campo3 = document.getElementById("n3");
 let resul = document.getElementById("alerta");
 
 btn.addEventListener("click", function(e){
     e.preventDefault();
-    if (campo1.value < campo2.value && campo1.value < campo3.value ){
-        resul.innerHTML = `El numero menor es ${campo1.value}`
-    } 
-    if (campo2.value < campo1.value && campo2.value < campo3.value ){
-        resul.innerHTML = `El numero menor es ${campo2.value}`
-    } 
-    if (campo3.value < campo1.value && campo3.value < campo2.value ){
-        resul.innerHTML = `El numero menor es ${campo3.value}`
-    } 
-
-    if (campo1.value < 101 && campo1.value > 0){
+    
+    if (campo1.value < 201 && campo1.value > 99){
         campo1.classList.remove("is-invalid");
         campo1.classList.add("is-valid");
     } else {
         campo1.classList.remove("is-valid");
         campo1.classList.add("is-invalid");
     }//campo1
-    if (campo2.value < 101 && campo2.value > 0){
-        campo2.classList.remove("is-invalid");
-        campo2.classList.add("is-valid");
-    } else {
-        campo2.classList.remove("is-valid");
-        campo2.classList.add("is-invalid");
-    }//campo2
-    if (campo3.value < 101 && campo3.value > 0){
-        campo3.classList.remove("is-invalid");
-        campo3.classList.add("is-valid");
-    } else {
-        campo3.classList.remove("is-valid");
-        campo3.classList.add("is-invalid");
-    }//campo3
 
-    
+
+    if((campo1.value % 3)==0){
+        resul.innerHTML=`${campo1.value} es multiplo de 3`
+    }else{
+        resul.innerHTML=`${campo1.value} no es multiplo de 3`
+    }
 })
